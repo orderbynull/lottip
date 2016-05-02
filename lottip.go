8b9f4ae163b67fae3158fbf12936bec326ea1d3e
Aleksandr Fedotov <mail@orderbynull.me>
2016-05-02 23:01:46 +0300

    Много улучшений

--- lottip.go
+++ lottip.go
@@ -7,143 +7,283 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"sync"
 
-	// "golang.org/x/text"
+	"fmt"
+
 	"github.com/gorilla/websocket"
+	// "golang.org/x/text"
 )
 
-func pipe(src, dst io.ReadWriter, dir bool, errsig chan bool, guichan chan string, guiReady chan bool) {
-	buff := make([]byte, 0xffff)
+//----------------------
+func ReadPacket(c net.Conn) ([]byte, error) {
+	var payload []byte
 	for {
-		n, err := src.Read(buff)
+		// Read packet header
+		var head = make([]byte, 4)
+		_, err := c.Read(head)
 		if err != nil {
-			//log.Print(err)
-			errsig <- true
-			return
-		}
-		b := buff[:n]
-
-		//write out result
-		n, err = dst.Write(b)
-		if err != nil {
-			//log.Print(err)
-			errsig <- true
-			return
+			return nil, fmt.Errorf("Cannot read header")
 		}
 
-		if dir == true {
-			cmd := buff[4]
-			if cmd == 0x03 {
-				//log.Printf("\n************************ \n\n %s \n\n************************ \n", buff)
+		// Packet Length [24 bit]
+		pktLen := int(uint32(head[0]) | uint32(head[1])<<8 | uint32(head[2])<<16)
+		log.Printf("PKTLEN = %d", pktLen)
+		if pktLen < 1 {
+			return nil, fmt.Errorf("pktLen < 1")
+		}
 
-				select {
-				case ready := <-guiReady:
-					if ready == true {
-						//log.Println("Sending to channel...")
-						guichan <- string(buff[5:n])
-					}
-				default:
+		// Read packet body [pktLen bytes]
+		var data = make([]byte, pktLen)
 
-				}
-			}
+		n, err := c.Read(data)
+		if err != nil {
+			return nil, fmt.Errorf("Cannot read payload")
+		}
+		log.Printf("READ = %d", n)
 
-			if cmd == 0x01 {
-				log.Print("COM_QUIT")
-			}
+		isLastPacket := (pktLen < MaxPayloadLen)
+
+		// Zero allocations for non-splitting packets
+		if isLastPacket && payload == nil {
+			log.Print("Non-splitting packet")
+			data = append(head, data...)
+			return data, nil
+		}
+
+		payload = append(payload, head...)
+		payload = append(payload, data...)
+
+		if isLastPacket {
+			log.Print("Splitted packet")
+			return payload, nil
 		}
 	}
 }
 
-type GuiStruct struct {
+//----------------------
+
+type GuiData struct {
 	Query string
 }
 
-var guiAddr = flag.String("gui", ":8080", "gui address")
+const MaxPayloadLen int = 1<<24 - 1
+
 var proxyAddr = flag.String("listen", "127.0.0.1:4040", "proxy address")
 var mysqlAddr = flag.String("mysql", "127.0.0.1:3306", "mysql address")
-
-var addr = flag.String("addr", "localhost:8080", "http service address")
+var guiAddr = flag.String("addr", "127.0.0.1:8080", "http service address")
+var verbose = flag.Bool("verbose", true, "mysql address")
 var upgrader = websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}
 
-func echo(w http.ResponseWriter, r *http.Request, guiReady chan bool, guiChan chan string) {
-	c, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Print("upgrade:", err)
-		return
-	}
-	defer c.Close()
+type Lottip struct {
+	wg        *sync.WaitGroup
+	gui       chan string
+	leftAddr  string
+	rightAddr string
+	verbose   bool
+}
 
-	data := GuiStruct{Query: ""}
-	for {
-		guiReady <- true
+func New(wg *sync.WaitGroup, leftAddr string, rightAddr string, verbose bool) *Lottip {
+	l := &Lottip{}
+	l.wg = wg
+	l.gui = make(chan string, 10)
+	l.leftAddr = leftAddr
+	l.rightAddr = rightAddr
+	l.verbose = verbose
 
-		data.Query = ""
+	return l
+}
 
-		data.Query = <-guiChan
-
-		json, _ := json.Marshal(data)
-
-		err = c.WriteMessage(1, json)
+func (l *Lottip) startWebsocket() {
+	http.Handle("/", http.FileServer(FS(false)))
+	http.HandleFunc("/proxy", func(w http.ResponseWriter, r *http.Request) {
+		c, err := upgrader.Upgrade(w, r, nil)
 		if err != nil {
-			log.Println("write:", err)
-			break
+			return
+		}
+		defer c.Close()
+
+		data := GuiData{}
+
+		for {
+			data.Query = <-l.gui
+
+			json, _ := json.Marshal(data)
+
+			err = c.WriteMessage(1, json)
+			if err != nil {
+				break
+			}
+		}
+	})
+
+	log.Fatal(http.ListenAndServe(*guiAddr, nil))
+}
+
+func (l *Lottip) startProxy() {
+	defer l.wg.Done()
+
+	sourceListener, err := net.Listen("tcp", l.leftAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sourceListener.Close()
+
+	for {
+		l.log("Waiting for LEFT connection")
+
+		leftConn, err := sourceListener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
 		}
 
+		l.log("Received LEFT connection")
+
+		go func(conn net.Conn) {
+			defer conn.Close()
+
+			var wg sync.WaitGroup
+
+			l.log("Handling LEFT connection")
+
+			rightConn, err := net.Dial("tcp", l.rightAddr)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			defer rightConn.Close()
+
+			wg.Add(2)
+			go l.leftToRight(conn, rightConn, &wg, true)
+			go l.leftToRight(rightConn, conn, &wg, false)
+			wg.Wait()
+
+			l.log("End of handling LEFT connection")
+		}(leftConn)
 	}
 }
 
-func main() {
-	flag.Parse()
+func (l *Lottip) log(msg string) {
+	if l.verbose {
+		log.Println(msg)
+	}
+}
 
-	guiChan := make(chan string, 1000)
-	guiReady := make(chan bool)
+func (l *Lottip) rightToLeft(right, left net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
 
-	go func() {
-
-		log.Println("Waiting for connection request for MySQL...")
-
-		sourceListener, err := net.Listen("tcp", *proxyAddr)
+	buf := make([]byte, 65535)
+	for {
+		n, err := left.Read(buf)
+		if n == 0 {
+			break
+		}
+		if err == io.EOF {
+			l.log("*** EOF ***")
+			break
+		}
 		if err != nil {
-			log.Fatal(err)
+			l.log("ERROR: " + err.Error())
+			break
 		}
 
+		_, err = right.Write(buf[0:n])
+		if err != nil {
+			l.log("3:" + err.Error())
+			break
+		}
+	}
+}
+
+func (l *Lottip) leftToRight(left, right net.Conn, wg *sync.WaitGroup, xx bool) {
+	defer wg.Done()
+
+	if xx {
 		for {
-			sourceConnection, err := sourceListener.Accept()
+			header := []byte{0, 0, 0, 0}
+
+			l.log("Waiting for data from SRC")
+			n, err := left.Read(header)
+			if n == 0 {
+				break
+			}
+			if err == io.EOF {
+				l.log("*** EOF ***")
+				break
+			}
 			if err != nil {
-				log.Print(err)
-				continue
+				l.log("ERROR: " + err.Error())
+				break
 			}
 
-			log.Println("Received connection request for MySQL")
+			sequence := uint8(header[3])
+			log.Printf("SEQ = %d", sequence)
 
-			errsig := make(chan bool)
+			length := int(uint32(header[0]) | uint32(header[1])<<8 | uint32(header[2])<<16)
 
-			func(sconnection net.Conn) {
-				defer sconnection.Close()
+			buf := make([]byte, length)
 
-				// log.Println("Dialing MySQL...");
+			bn, err := left.Read(buf)
+			if bn == 0 {
+				break
+			}
+			if err == io.EOF {
+				l.log("*** EOF ***")
+				break
+			}
+			if err != nil {
+				l.log("ERROR: " + err.Error())
+				break
+			}
 
-				destinationConnection, err := net.Dial("tcp", *mysqlAddr)
-				if err != nil {
-					log.Println(err)
-                    return
+			// log.Printf("READ = %d", bn)
+			// if bn != length {
+			// 	l.log("!!!!!!!!!!!!!!")
+			// }
+
+			_, err = right.Write(append(header, buf[0:bn]...))
+			if err != nil {
+				l.log("3:" + err.Error())
+				break
+			}
+
+			if buf[0] == 0x03 && xx {
+
+				select {
+				case l.gui <- string(buf[1:bn]):
+				default:
 				}
-
-				//log.Println("Connection to MySQL succedded")
-
-				defer destinationConnection.Close()
-
-				go pipe(sconnection, destinationConnection, true, errsig, guiChan, guiReady)
-				go pipe(destinationConnection, sconnection, false, errsig, guiChan, guiReady)
-
-				<-errsig
-			}(sourceConnection)
+			}
 		}
-	}()
+	} else {
+		for {
+			buf := make([]byte, 65535)
+			n, err := left.Read(buf)
+			if err != nil {
+				log.Println("Error: "+err.Error())
+				break
+			}
+			
+			right.Write(buf[:n])
+		}
+	}
+}
 
-    http.Handle("/", http.FileServer(FS(false)))
-	http.HandleFunc("/proxy", func(w http.ResponseWriter, r *http.Request) {
-		echo(w, r, guiReady, guiChan)
-	})
-	log.Fatal(http.ListenAndServe(*addr, nil))
+func (l *Lottip) Run() {
+	go l.startWebsocket()
+	go l.startProxy()
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	flag.Parse()
+
+	wg.Add(1)
+
+	lottip := New(&wg, *proxyAddr, *mysqlAddr, *verbose)
+	lottip.Run()
+
+	wg.Wait()
 }
--- web/js/app.js
+++ web/js/app.js
@@ -5,9 +5,9 @@ $(document).ready(function () {
     var vm = new Vue({
         el: '#app',
         data: {
-            showId: true,
-            showTime: true,
-            isTruncated: true,
+            showId: false,
+            showTime: false,
+            isExpanded: true,
             isConnected: false,
             items: []
         },
@@ -20,13 +20,16 @@ $(document).ready(function () {
                 this.showTime = !this.showTime;
             },
 
-            toggleTruncated: function () {
-                this.isTruncated = !this.isTruncated;
+            toggleExpanded: function () {
+                var v = this;
+                this.isExpanded = !this.isExpanded;
+                this.items.forEach(function(item, index){
+                    item.detailed = v.isExpanded;
+                });
             },
 
             showDetails: function (index) {
-                var showDetailed = this.items[index].detailed;
-                this.items[index].detailed = !showDetailed;
+                this.items[index].detailed = !this.items[index].detailed;
             },
 
             clearItems: function () {
@@ -47,8 +50,8 @@ $(document).ready(function () {
                     vue.isConnected = true;
                 }
 
-                ws.onclose = function (evt) {
-                    console.log('DISCONNECTED');
+                ws.onclose = function (evt, reason) {
+                    console.log('DISCONNECTED '+reason);
                     vue.isConnected = false;
                     ws = null;
                 }
@@ -56,7 +59,7 @@ $(document).ready(function () {
                 ws.onmessage = function (evt) {
                     data = jQuery.parseJSON(evt.data);
 
-                    vue.items.push({time: new Date().toLocaleTimeString(), query: data.Query, detailed: false});
+                    vue.items.push({time: new Date().toLocaleTimeString(), query: data.Query, detailed: vm.isExpanded});
                 }
 
                 ws.onerror = function (evt) {
@@ -75,6 +78,6 @@ $(document).ready(function () {
         }
     });
     
-   vm.items.push({time: new Date().toLocaleTimeString(), query: "SELECT self.*,self.name_ru as name, self.map_img_ext_ru as map_img_ext, resort.name_ru as resort_name, resort.name_en as resort_name_en, resort.country as country, country.name_ru as country_name, country.name_en as country_name_en, country.name_en as country_name_eng, country.region as region, country.sng as sng, country.sng as is_sng, country.iso_code as country_iso_code FROM uts_city as self LEFT JOIN uts_resort resort ON resort.id = self.resort LEFT JOIN uts_country country ON country.id = resort.country WHERE self.id = '4' AND self.name_ru<>'' AND self.name_ru IS NOT NULL AND self.name_en<>'' AND self.name_en IS NOT NULL LIMIT 1", detailed: false});
+//    vm.items.push({time: new Date().toLocaleTimeString('en-US', { hour12: false }), query: "SELECT self.*,self.name_ru as name, self.map_img_ext_ru as map_img_ext, resort.name_ru as resort_name, resort.name_en as resort_name_en, resort.country as country, country.name_ru as country_name, country.name_en as country_name_en, country.name_en as country_name_eng, country.region as region, country.sng as sng, country.sng as is_sng, country.iso_code as country_iso_code FROM uts_city as self LEFT JOIN uts_resort resort ON resort.id = self.resort LEFT JOIN uts_country country ON country.id = resort.country WHERE self.id = '4' AND self.name_ru<>'' AND self.name_ru IS NOT NULL AND self.name_en<>'' AND self.name_en IS NOT NULL LIMIT 1", detailed: false});
 
 });
