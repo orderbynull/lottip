35e228ffe1aedc2dd297a6b0f083c71e5afb7aaf
Aleksandr Fedotov <mail@orderbynull.me>
2016-07-14 23:06:30 +0300

    Added comments and install instructions

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# lottip
+# Lottip
 
-MySQL proxy server with browser based GUI
+MySQL proxy server with browser based GUI.
+**Under heavy development.**
+
+# Installation
+
+1) Build static file to embed into binary: `./esc -o static.go -prefix web -ignore="\.idea.*|\.DS.*" web`
+2) Buld with `static.go`: `go build lottip.go static.go`
+3) Run: `./lottip --listen=:5050 --mysql=192.168.0.195:3306`
--- lottip.go
+++ lottip.go
@@ -13,7 +13,7 @@ import (
 	"github.com/orderbynull/lottip/mysql"
 )
 
-// GuiData holds data sent to browser
+// GuiData holds data sent to browser via websocket
 type GuiData struct {
 	Query      string
 	NewSession bool
@@ -21,6 +21,7 @@ type GuiData struct {
 	Type       string
 }
 
+//SessionState ...
 type SessionState struct {
 	SessionID int
 	State     bool
@@ -33,7 +34,7 @@ var guiAddr = flag.String("addr", "127.0.0.1:8080", "http service address")
 var verbose = flag.Bool("verbose", true, "verbose mode")
 var upgrader = websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}
 
-//Lottip defines application
+//Lottip defines application structure
 type Lottip struct {
 	wg        *sync.WaitGroup
 	gui       chan GuiData
@@ -66,15 +67,19 @@ func (l *Lottip) Run() {
 func (l *Lottip) StartWebsocket() {
 	http.Handle("/", http.FileServer(FS(false)))
 	http.HandleFunc("/proxy", func(w http.ResponseWriter, r *http.Request) {
+
+		//Init websocket connection
 		c, err := upgrader.Upgrade(w, r, nil)
 		if err != nil {
 			return
 		}
 		defer c.Close()
 
+		//Holds data to be sent via websocket
 		var data []byte
 
 		for {
+			//Reading data came for gui and preparing to send
 			select {
 			case q := <-l.gui:
 				data, _ = json.Marshal(q)
@@ -85,6 +90,7 @@ func (l *Lottip) StartWebsocket() {
 				break
 			}
 
+			//Pushing data to gui
 			err = c.WriteMessage(1, data)
 			if err != nil {
 				l.log("Error writing to socket: " + err.Error())
@@ -100,23 +106,28 @@ func (l *Lottip) StartWebsocket() {
 func (l *Lottip) StartProxy() {
 	defer l.wg.Done()
 
+	//Listening for connection from client
 	sourceListener, err := net.Listen("tcp", l.leftAddr)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer sourceListener.Close()
 
+	//Start counting sessions
 	sessionID := 1
 
 	for {
+		//Accepting connection from client
 		leftConn, err := sourceListener.Accept()
 		if err != nil {
 			log.Print(err)
 			continue
 		}
 
+		//Parse and proxy packets to server
 		go l.HandleConnection(leftConn, sessionID)
 
+		//Each new connection from client means new session
 		sessionID++
 	}
 }
@@ -126,10 +137,12 @@ func (l *Lottip) HandleConnection(leftConn net.Conn, sessionID int) {
 	defer leftConn.Close()
 	defer l.SessionEnded(sessionID)
 
+	//Send "session started" event to gui
 	l.SessionStarted(sessionID)
 
 	var wg sync.WaitGroup
 
+	//Trying to connect to server
 	rightConn, err := net.Dial("tcp", l.rightAddr)
 	if err != nil {
 		return
@@ -137,11 +150,13 @@ func (l *Lottip) HandleConnection(leftConn net.Conn, sessionID int) {
 	defer rightConn.Close()
 
 	wg.Add(2)
+	//Start passing packets from client to server
 	go func() {
 		defer wg.Done()
 		l.LeftToRight(leftConn, rightConn, sessionID)
 	}()
 
+	//Start passing packets from server to client
 	go func() {
 		defer wg.Done()
 		l.RightToLeft(rightConn, leftConn)
@@ -155,17 +170,17 @@ func (l *Lottip) log(msg string) {
 	}
 }
 
-//SessionStarted ...
+//SessionStarted sends "session started" event to gui
 func (l *Lottip) SessionStarted(sessID int) {
 	l.sessions <- SessionState{SessionID: sessID, State: true, Type: "State"}
 }
 
-//SessionEnded ...
+//SessionEnded sends "session ended" event to gui
 func (l *Lottip) SessionEnded(sessID int) {
 	l.sessions <- SessionState{SessionID: sessID, State: false, Type: "State"}
 }
 
-//RightToLeft ...
+//RightToLeft passes packets from server to client
 func (l *Lottip) RightToLeft(left, right net.Conn) {
 	for {
 		buf := make([]byte, 65535)
@@ -179,8 +194,9 @@ func (l *Lottip) RightToLeft(left, right net.Conn) {
 	}
 }
 
-//LeftToRight ...
+//LeftToRight passes packets from client to server
 func (l *Lottip) LeftToRight(left, right net.Conn, sessID int) {
+
 	//Indicates first query in session
 	//First query means session just started
 	isNewSession := true
