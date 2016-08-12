fdcd3fac2c47655cd229bc0632c385e54ec908a6
Aleksandr Fedotov <mail@orderbynull.me>
2016-08-12 09:29:04 +0300

    Const definitions in idiomatic way

--- mysql/const.go
+++ mysql/const.go
@@ -1,69 +1,77 @@
 package mysql
 
-//ComSleep is internal server command
-const ComSleep byte = 0x00
+const (
+	//ComSleep is internal server command
+	ComSleep byte = iota
 
-//ComQuit tells the server that the client wants to close the connection
-const ComQuit byte = 0x01
+	//ComQuit tells the server that the client wants to close the connection
+	ComQuit
 
-//ComInitDb changes the default schema of the connection
-const ComInitDb byte = 0x02
+	//ComInitDB changes the default schema of the connection
+	ComInitDB
 
-//ComQuery is used to send the server a text-based query that is executed immediately
-const ComQuery byte = 0x03
+	//ComQuery is used to send the server a text-based query that is executed immediately
+	ComQuery
 
-//ComFieldList gets the column definitions of a table
-//As of MySQL 5.7.11, ComFieldList is deprecated and will be removed in a future version of MySQL
-const ComFieldList byte = 0x04
+	//ComFieldList gets the column definitions of a table
+	//As of MySQL 5.7.11, ComFieldList is deprecated and will be removed in a future version of MySQL
+	ComFieldList
 
-//ComCreateDb creates a schema
-const ComCreateDb byte = 0x05
+	//ComCreateDb creates a schema
+	ComCreateDb
 
-//ComDropDb drops a schema
-const ComDropDb byte = 0x06
+	//ComDropDB drops a schema
+	ComDropDB
 
-//ComRefresh is low-level version of several FLUSH ... and RESET ... statements
-//As of MySQL 5.7.11, ComRefresh is deprecated and will be removed in a future version of MySQL
-const ComRefresh byte = 0x07
+	//ComRefresh is low-level version of several FLUSH ... and RESET ... statements
+	//As of MySQL 5.7.11, ComRefresh is deprecated and will be removed in a future version of MySQL
+	ComRefresh
 
-//ComShutdown is used to shut down the MySQL server
-//As of MySQL 5.7.9, ComShutdown is deprecated and will be removed in MySQL 8.0
-const ComShutdown byte = 0x08
+	//ComShutdown is used to shut down the MySQL server
+	//As of MySQL 5.7.9, ComShutdown is deprecated and will be removed in MySQL 8.0
+	ComShutdown
 
-//ComStatistics gets a human readable string of internal statistics
-const ComStatistics byte = 0x09
+	//ComStatistics gets a human readable string of internal statistics
+	ComStatistics
 
-//ComProcessInfo gets a list of active threads
-//As of MySQL 5.7.11, ComProcessInfo is deprecated and will be removed in a future version of MySQL
-const ComProcessInfo byte = 0x0a
+	//ComProcessInfo gets a list of active threads
+	//As of MySQL 5.7.11, ComProcessInfo is deprecated and will be removed in a future version of MySQL
+	ComProcessInfo
 
-//ComConnect is an internal command in the server
-const ComConnect byte = 0x0b
+	//ComConnect is an internal command in the server
+	ComConnect
 
-//ComProcessKill asks the server to terminate a connection
-//As of MySQL 5.7.11, ComProcessKill is deprecated and will be removed in a future version of MySQL
-const ComProcessKill byte = 0x0c
+	//ComProcessKill asks the server to terminate a connection
+	//As of MySQL 5.7.11, ComProcessKill is deprecated and will be removed in a future version of MySQL
+	ComProcessKill
 
-//ComDebug triggers a dump on internal debug info to stdout of the mysql-server
-const ComDebug byte = 0x0d
+	//ComDebug triggers a dump on internal debug info to stdout of the mysql-server
+	ComDebug
 
-//ComPing checks if the server is alive
-const ComPing byte = 0x0e
+	//ComPing checks if the server is alive
+	ComPing
 
-//ComTime is an internal command in the server
-const ComTime byte = 0x0f
+	//ComTime is an internal command in the server
+	ComTime
 
-//ComDelayedInsert is an internal command in the server
-const ComDelayedInsert byte = 0x10
+	//ComDelayedInsert is an internal command in the server
+	ComDelayedInsert
 
-//ComChangeUser changes the user of the current connection and reset the connection state
-const ComChangeUser byte = 0x11
+	//ComChangeUser changes the user of the current connection and reset the connection state
+	ComChangeUser
 
-//ComResetConnection resets the session state
-const ComResetConnection byte = 0x1f
-
-//ComDaemon is an internal command in the server
-const ComDaemon byte = 0x1d
+	ComBinlogDump
+	ComTableDump
+	ComConnectOut
+	ComRegisterSlave
+	ComStmtPrepare
+	ComStmtExecute
+	ComStmtSendLongData
+	ComStmtClose
+	ComStmtReset
+	ComSetOption
+	ComStmtFetch
+)
 
 //MaxPacketSize is max size allowed for packet
 const MaxPacketSize = 1<<24 - 1
