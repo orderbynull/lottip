a9815d1bd953ccabec6ae0437dfec563ad4813a9
Aleksandr Fedotov <mail@orderbynull.me>
2019-12-22 00:04:39 +0300

    Basic templates

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main()  {
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		tmpl := template.Must(template.ParseFiles("ui/templates/layout.html", "ui/templates/default.html"))
+		if err := tmpl.ExecuteTemplate(writer, "layout", nil); err != nil {
+			log.Println(err)
+		}
+	})
+
+	http.HandleFunc("/mysql", func(writer http.ResponseWriter, request *http.Request) {
+		tmpl := template.Must(template.ParseFiles("ui/templates/layout.html", "ui/templates/mysql.html"))
+		if err := tmpl.ExecuteTemplate(writer, "layout", nil); err != nil {
+			log.Println(err)
+		}
+	})
+
+	http.HandleFunc("/pgsql", func(writer http.ResponseWriter, request *http.Request) {
+		tmpl := template.Must(template.ParseFiles("ui/templates/layout.html", "ui/templates/pgsql.html"))
+		if err := tmpl.ExecuteTemplate(writer, "layout", nil); err != nil {
+			log.Println(err)
+		}
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
